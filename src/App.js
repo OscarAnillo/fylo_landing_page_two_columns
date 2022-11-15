e43a47fc10245080aee5094ed88b46dc2bd2863c
oscar anillo <oscarani14@gmail.com>
2022-11-15 14:24:01 -0500

    add hero component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeroComponent from "./Components/hero-component";
 import NavComponent from "./Components/nav-component";
 
 function App() {
   return (
     <div className="App">
       <NavComponent />
+      <HeroComponent />
     </div>
   );
 }
---
+++ src/Components/hero-component.js
@@ -0,0 +1,25 @@
+export default function HeroComponent() {
+  return (
+    <section className="hero">
+      <div className="hero-container">
+        <div>
+          <img src="images/illustration-1.svg" alt="" />
+        </div>
+        <div>
+          <h1 className="hero-title">
+            All your files in one secure location, accessible anywhere.
+          </h1>
+          <p className="hero-text">
+            Fylos stores your most important files in one secure location.
+            Access them wherever you need, share and collaborate with friends,
+            family, and co-workers.
+          </p>
+          <form>
+            <input type="text" placeholder="Enter your email..." />
+            <button>Get Started</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+}
