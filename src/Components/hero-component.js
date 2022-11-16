3736923af56ea8b32fcab20a1435357e4094eea2
oscar anillo <oscarani14@gmail.com>
2022-11-16 12:28:59 -0500

    add email validation

--- src/Components/hero-component.js
+++ src/Components/hero-component.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 
 export default function HeroComponent() {
@@ -8,6 +8,11 @@ export default function HeroComponent() {
   const textHeroRef = useRef(null);
   const inputHeroRef = useRef(null);
   const buttonHeroRef = useRef(null);
+
+  const [emailInput, setEmailInput] = useState({ email: "" });
+  const [emailError, setEmailError] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
 
   useEffect(() => {
     tl.from("#one-hero", { opacity: 0, y: 40, duration: 0.5 });
@@ -66,6 +71,38 @@ export default function HeroComponent() {
     );
   }, [tl]);
 
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setEmailInput({ ...emailInput, [name]: value });
+  };
+
+  const validate = (value) => {
+    const error = {};
+
+    if (!value.email) {
+      error.email = "An email is required";
+    } else if (!emailRegex.test(value.email)) {
+      error.email = "Please enter a valid email address";
+    }
+    return error;
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (emailRegex.test(emailInput.email)) {
+      alert(`We register your email as ${emailInput.email}`);
+      setEmailInput({ email: "" });
+    }
+    setEmailError(validate(emailInput));
+    setIsSubmitted(true);
+  };
+
+  useEffect(() => {
+    if (Object.keys(emailInput).length === 0 && isSubmitted) {
+      console.log("email");
+    }
+  }, [emailInput, isSubmitted]);
+
   return (
     <section className="hero">
       <div className="hero-container">
@@ -86,17 +123,21 @@ export default function HeroComponent() {
             them wherever you need, share and collaborate with friends, family,
             and co-workers.
           </p>
-          <form>
+          <form onSubmit={submitHandler} autoComplete="off">
             <input
               type="text"
+              name="email"
               placeholder="Enter your email..."
               ref={inputHeroRef}
               className="input"
+              value={emailInput.email}
+              onChange={changeHandler}
             />
             <button ref={buttonHeroRef} className="button">
               Get Started
             </button>
           </form>
+          {emailError.email && <p className="error">{emailError.email}</p>}
         </div>
       </div>
     </section>
